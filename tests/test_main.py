533b813569c283ec3cac76d955600f4342e6f734
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2022-04-09 21:38:12 +0100

    fix tests (further work needed)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,12 @@
 """Test the image tracer."""
+import sys
 from pathlib import Path
 
-from svgtrace import trace
-
 THISDIR = str(Path(__file__).resolve().parent)
+sys.path.insert(0, str(Path(THISDIR).parent))
 logoFile = f"{THISDIR}/data/logo"
+
+from svgtrace import trace
 
 Path(f"{logoFile}-bw.svg").write_text(trace(f"{logoFile}-bw.png", True), encoding="utf-8")
 
